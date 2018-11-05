2831f11a6f0f29f46fb042c6746579e9fc70cbe2
asif9 <asif9@LAPTOP-Asif>
2018-11-05 21:18:51 +0530

    Comminting all for frst time.

---
+++ ThreadDemo/src/ThreadDemo.java
@@ -0,0 +1,55 @@
+
+
+class  increment 
+{
+int count;
+public synchronized void inc()
+{
+count++;
+}
+}
+
+
+public class ThreadDemo {
+
+	public static void main(String[] args) throws Exception{
+
+		increment in =new increment();
+		
+		double as=3_5_2.0;
+		float ef=(float) 2_123456;
+		System.out.println("as :"+as+"ef"+ef);
+		
+		int corecount =Runtime.getRuntime().availableProcessors();
+		System.out.println("core count"+corecount);
+		Thread t1=new Thread(new Runnable () {
+			public void run() 
+			{
+				for(int i=0;i<1000;i++)
+				{
+					in.inc();
+				}
+				
+			}
+			
+		});
+		Thread t2=new Thread(new Runnable () {
+			public void run() 
+			{
+				for(int i=0;i<1000;i++)
+				{
+					in.inc();
+				}
+				
+			}
+			
+		});
+		t1.start();
+		t2.start();
+		t1.join();
+		t2.join();
+		System.out.println("count value is "+ in.count);
+
+	}
+
+}
